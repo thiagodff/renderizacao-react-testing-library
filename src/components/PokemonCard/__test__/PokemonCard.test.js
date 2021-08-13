b0ef513eae5980f5774ef1bd1a333ff5273cef03
Thiago Dornelles <thiago.fdornelles@gmail.com>
2021-08-13 07:39:34 -0300

    test: PokemonCard component

--- src/components/PokemonCard/__test__/PokemonCard.test.js
+++ src/components/PokemonCard/__test__/PokemonCard.test.js
@@ -1,1 +1,42 @@
-// TODO testar component
+import React from 'react';
+import { render, fireEvent } from '@testing-library/react';
+
+import PokemonCard from '../PokemonCard';
+import { cardBuilder } from '../../../__mocks__/card-builder';
+
+const setup = (props = {
+	card: cardBuilder({ name: 'MeuPokemon' })
+}) => {
+	const { card } = props;
+	const imageAlt = `${card.id}-${card.name}`;
+	// const onClick = jest.fn(() => { podemos fazer algo quando o evento é emitido });
+	const onClick = jest.fn();
+	const renderResult = render(<PokemonCard {...card} onClick={onClick} />);
+	const imageElement = renderResult.getByAltText(imageAlt);
+
+	return {
+		card,
+		imageAlt,
+		imageElement,
+		onClick,
+		...renderResult,
+	}
+}
+
+describe('PokemonCard', () => {
+	test('should render with default props', () => {
+		const { card, imageAlt, imageElement } = setup();
+		
+		expect(imageElement).toBeInTheDocument();
+		expect(imageElement.src).toBe(card.imageUrl);
+		expect(imageElement.alt).toBe(imageAlt);
+	});
+	
+	test('should emit event onClick', () => {
+		const { onClick, imageElement } = setup();
+		// fireEvent.click(imageElement);
+		imageElement.click();
+
+		expect(onClick).toHaveBeenCalledTimes(1);
+	})
+})
